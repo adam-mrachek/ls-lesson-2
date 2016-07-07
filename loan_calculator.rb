688d572f310cd5cef65b923218040e20a6567310
adamj <amrachek@outlook.com>
2016-07-07 13:29:36 -0600

    Updated with a couple changes to improve user interaction

--- loan_calculator.rb
+++ loan_calculator.rb
@@ -1,3 +1,7 @@
+def prompt(message)
+  puts ">>> #{message}"
+end
+
 def valid_number?(num)
   num.to_i > 0 && num.to_i != 0
 end
@@ -8,7 +12,7 @@ puts "This calculator will estimate your monthly mortgage payment.".center(60)
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".center(60)
 
 loop do
-  puts "To start, please enter the total loan amount:"
+  prompt "To start, please enter the total loan amount:"
 
   loan_amount = ''
   loop do
@@ -17,33 +21,33 @@ loop do
     if valid_number?(loan_amount)
       break
     else
-      puts "Please enter a valid loan amount."
-      puts "(Numbers only. No letters or special characters.)"
+      prompt "Please enter a valid loan amount."
+      prompt "(Numbers only. No letters or special characters.)"
     end
   end
 
   apr = ''
   loop do
-    puts "Next, enter the Annual Percentage Rate (APR) for your loan:"
-    puts "If your rate is 5%, enter 5. If your rate is 2.5%, enter 2.5."
+    prompt "Next, enter the Annual Percentage Rate (APR) for your loan:"
+    prompt "If your rate is 5%, enter 5. If your rate is 2.5%, enter 2.5."
     apr = gets.chomp.delete('%').to_f
 
     if valid_number?(apr)
       break
     else
-      puts "Hmm, it looks like you entered an invalid interest rate."
+      prompt "Hmm, it looks like you entered an invalid interest rate."
     end
   end
 
   loan_years = ''
   loop do
-    puts "Lastly, enter the loan duration in years:"
+    prompt "Lastly, enter the loan duration in years:"
     loan_years = gets.chomp.to_i
 
     if valid_number?(loan_years)
       break
     else
-      puts "Hmmm, that doesn't appear to be a valid number."
+      prompt "Hmmm, that doesn't appear to be a valid number."
     end
   end
 
@@ -53,9 +57,9 @@ loop do
                     (monthly_rate * (1 + monthly_rate)**loan_months) /
                     ((1 + monthly_rate)**loan_months - 1)
 
-  puts "Your monthly payment will be: $#{monthly_payment.to_f.round(2)}"
-
-  puts "Would you like to do another calculation?"
+  prompt "Your monthly payment will be: $#{monthly_payment.to_f.round(2)}"
+  puts ""
+  prompt "Would you like to do another calculation? (y/n)"
   response = gets.chomp.downcase
 
   break unless response.start_with?('y')
